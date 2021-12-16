514df669529f6e459f39e85a389c0436f83dc0dc
unknown <jhonmurillousuga33@gmail.com>
2021-12-16 01:19:40 -0500

    Se agrega listado de solicitudes de prestamo

--- src/main/java/com/mercadolibre/prueba/persistence/repositories/LoanApplicationRepository.java
+++ src/main/java/com/mercadolibre/prueba/persistence/repositories/LoanApplicationRepository.java
@@ -1,9 +1,17 @@
 package com.mercadolibre.prueba.persistence.repositories;
 
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.mercadolibre.prueba.model.LoanApplication;
 
 public interface LoanApplicationRepository extends JpaRepository<LoanApplication, Long>{
+	
+	@Query("from LoanApplication lap where lap.date between :from and :to")
+	List<LoanApplication> findLoanAplicationsBetweenDates(@Param("from")Date from,@Param("to")Date to);
 
 }
--- src/main/java/com/mercadolibre/prueba/service/ILoanApplicationService.java
+++ src/main/java/com/mercadolibre/prueba/service/ILoanApplicationService.java
@@ -1,10 +1,14 @@
 package com.mercadolibre.prueba.service;
 
+import java.util.Date;
+import java.util.List;
+
 import com.mercadolibre.prueba.controller.dto.LoanApplicationDTO;
 import com.mercadolibre.prueba.controller.dto.response.FeeDTO;
 
 public interface ILoanApplicationService {
 	
-	FeeDTO registerLoanApplication(LoanApplicationDTO dto);
+	public FeeDTO registerLoanApplication(LoanApplicationDTO dto);
+	public List<LoanApplicationDTO> getLoanApplications(Date from, Date to);
 
 }
