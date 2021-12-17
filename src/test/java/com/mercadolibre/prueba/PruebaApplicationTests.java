60e4700b01300fb5cd420c677241de3054dfd3e0
unknown <jhonmurillousuga33@gmail.com>
2021-12-16 22:52:43 -0500

    Se realiza cambio para evitar conexion con la base de datos en los test

--- src/test/java/com/mercadolibre/prueba/PruebaApplicationTests.java
+++ src/test/java/com/mercadolibre/prueba/PruebaApplicationTests.java
@@ -3,11 +3,11 @@ package com.mercadolibre.prueba;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class PruebaApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
