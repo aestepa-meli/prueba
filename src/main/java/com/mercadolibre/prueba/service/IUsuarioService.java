02dd3c9d433855872e760102af5be5b0b1c66222
unknown <jhonmurillousuga33@gmail.com>
2021-12-16 22:35:16 -0500

    Se crea modulo de registro de usuario solo para facilitar pruebas

--- src/main/java/com/mercadolibre/prueba/service/IUsuarioService.java
+++ src/main/java/com/mercadolibre/prueba/service/IUsuarioService.java
@@ -1,5 +1,7 @@
 package com.mercadolibre.prueba.service;
 
+import com.mercadolibre.prueba.controller.dto.UserDTO;
+
 /**
  * <p>IUsuarioService</p>
  * Interfaz de servicio para el manejo de usuarios
@@ -14,4 +16,10 @@ public interface IUsuarioService {
 	 * @return valor booleando indicando si el usuario es valido
 	 */
 	boolean isValidUser(String user, String password);
+	
+	/**
+	 * Registro de usuarios
+	 * @param userDTO
+	 */
+	void register(UserDTO userDTO);
 }
